75e6feb83cc37600ddc45fe923e3bf9db1cb2567
jak002 <147598891+jak002@users.noreply.github.com>
2024-10-29 13:34:05 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Empty vue.js template',
+            undertitle: 'Ripe for customization'
+        }
+    },
+    methods: {
+        defaultMethod(){
+
+        }
+    },
+    computed: {
+        defaultComputed(){
+            return ''
+        },
+
+    }
+})
